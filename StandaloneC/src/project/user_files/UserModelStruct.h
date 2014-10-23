46e3b3140a9e5e4420d0dc01d2ba12c2e33d5a29
Timothee Habra <timothee.habra@uclouvain.be>
2014-10-23 15:39:21 +0200

    add 2D constraints for new xml reading (basic_version of coman)

--- StandaloneC/src/project/user_files/UserModelStruct.h
+++ StandaloneC/src/project/user_files/UserModelStruct.h
@@ -2,7 +2,7 @@
 // C-code automatically generated from Gen_mds_user project
 //
 //
-// Last update : Thu Oct 23 11:42:04 2014
+// Last update : Thu Oct 23 14:21:00 2014
 //---------------------------
 
 
